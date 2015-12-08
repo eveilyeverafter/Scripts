0e5fab1b295a6922b4d1c3b125295b52d88a3a99
Tyler Hether <heth8145@vandals.uidaho.edu>
2015-12-08 15:06:56 -0800

    updated alignment script for new file structure

--- 02-RecombinationRates/PAR/03-RunAlignment.sh
+++ 02-RecombinationRates/PAR/03-RunAlignment.sh
@@ -7,10 +7,9 @@ NR_CPUS=14
 while read line
     do
     set 'echo $line'
-    bowtie2 -p 1 -x ../00-Scripts/bowtie2indeces/s288cIndex -U ../02-Cleaned/"$line"/"$line"_merged_SE.fastq > "$line"_aligned.se.sam &   
-    bowtie2 -p 1 -x ../00-Scripts/bowtie2indeces/s288cIndex -1 ../02-Cleaned/"$line"/"$line"_notcombined_PE1.fastq -2 ../02-Cleaned/"$line"/"$line"_notcombined_PE2.fastq -I 400 -X 800 > "$line"_aligned.pe.sam & 
+    bowtie2 -p 1 -x "$SCRIPTS"/reference_seq/bowtie2indeces/s288cIndex -U ../02-Cleaned/"$line"/"$line"_merged_SE.fastq > "$line"_aligned.se.sam &   
+    bowtie2 -p 1 -x "$SCRIPTS"/reference_seq/bowtie2indeces/s288cIndex -1 ../02-Cleaned/"$line"/"$line"_notcombined_PE1.fastq -2 ../02-Cleaned/"$line"/"$line"_notcombined_PE2.fastq -I 400 -X 800 > "$line"_aligned.pe.sam & 
     let count+=2
     [[ $((count%NR_CPUS)) -eq 0 ]] && wait  
 done < dirs
 ###################
-

c9f36b38dbc594aa5874972e52c334abb45ae49f
Tyler Hether <heth8145@vandals.uidaho.edu>
2015-12-09 10:27:54 -0800

    script to subset vcf files to the locilist

--- 02-RecombinationRates/RAD_DIP/06-TrimaAndParse.sh
+++ 02-RecombinationRates/RAD_DIP/06-TrimaAndParse.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# TrimAndParse.sh
-
-# SNPLIST="../../00-First_Miseq_14_tetrads/03-Alignments/YPS128_DBVPG1106_snps.noMultiBase.reformatted.txt"
-# cat $SNPLIST | sed 's/\s\s*/ /g' | cut -d' ' -f1-2 > locilist
 
 
 for plate in `seq 1 4`; 
@@ -11,21 +7,20 @@ do
         while read line
                 do
                 set 'echo $line'
-                bcftools view -ce -l ../../../00-WGS_Q00/04-Parents/locilist ../../01-Processing/03-CallVariants/Plate"$plate"/"$line"_Yvar.raw.bcf > "$line"_Yvar.trim.vcf
-                bcftools view -ce -l ../../../00-WGS_Q00/04-Parents/locilist ../../01-Processing/03-CallVariants/Plate"$plate"/"$line"_Dvar.raw.bcf  > "$line"_Dvar.trim.vcf   
-        done < ../../00-Scripts/barcodes1504.txt
+                bcftools view -ce -l "$SNPDIR"/locilist ../../01-Processing/03-CallVariants/Plate"$plate"/"$line"_Yvar.raw.bcf > "$line"_Yvar.trim.vcf
+                bcftools view -ce -l "$SNPDIR"/locilist ../../01-Processing/03-CallVariants/Plate"$plate"/"$line"_Dvar.raw.bcf  > "$line"_Dvar.trim.vcf   
+        done < "$SCRIPTS"/02-RecombinationRates/RAD_DIP/barcodes1504.txt
 
         while read line
                 do
                 set 'echo $line'
-                ../../00-Scripts/ParseVCF2.pl "$line"_Yvar.trim.vcf > "$line"_Ycounts.txt 
-                ../../00-Scripts/ParseVCF2.pl "$line"_Dvar.trim.vcf > "$line"_Dcounts    
-        done < ../../00-Scripts/barcodes1504.txt
+                "$SCRIPTS"/02-RecombinationRates/ParseVCF2.pl "$line"_Yvar.trim.vcf > "$line"_Ycounts.txt 
+                "$SCRIPTS"/02-RecombinationRates/ParseVCF2.pl "$line"_Dvar.trim.vcf > "$line"_Dcounts    
+        done < "$SCRIPTS"/02-RecombinationRates/RAD_DIP/barcodes1504.txt
         cd ../
 done
 
 
 
 echo 'Data ready for analysis in R. '
-
 

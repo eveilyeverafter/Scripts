ed5258d3f8d788b06de04d9522e81bd338ee9377
Tyler Hether <heth8145@vandals.uidaho.edu>
2015-12-08 16:00:17 -0800

    script to convert multiline fasta reads to singleline format

--- 02-RecombinationRates/singleline.pl
+++ 02-RecombinationRates/singleline.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-#perl fa2oneline.pl sample.fa > out.fa
+# perl fa2oneline.pl sample.fa > out.fa
 # downloaded from http://www.bioinformatics-made-simple.com
 # change multiline fasta to single line fasta
 use strict;
